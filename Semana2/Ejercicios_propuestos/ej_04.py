abc336c9855b90dc8b47fd1b634d1ce034251d03
unknown <johanacent@gmail.com>
2023-05-06 06:02:22 -0300

    Semana 2

--- Semana2/Ejercicios_propuestos/ej_04.py
+++ Semana2/Ejercicios_propuestos/ej_04.py
@@ -1,2 +1,10 @@
 # Crear un diccionario con los nombres de tres personas y sus respectivas edades. 
 # Mostrar la edad de la tercera persona en el diccionario
+
+personas = {
+    "María" : "15 años",
+    "Germán" : "8 años",
+    "Juan" : "50 años"
+}
+
+print(personas["Juan"])
