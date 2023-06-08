98f956a58e8cc94e40f4c564116407cc7e13fa24
Johana <131535999+JbhC@users.noreply.github.com>
2023-06-08 20:57:26 -0300

    Update desafio_07.sql

--- SEMANA7/desafio_07.sql
+++ SEMANA7/desafio_07.sql
@@ -4,10 +4,10 @@ USE desafio_07;
 
 CREATE TABLE comentario (
     
-    id INT PRIMARY KEY; 
-    id_articulo int(6);
-    id_usuario INT (4);
-    contenido VARCHAR(255);
-    fecha_hora DATETIME;
+    id INT PRIMARY KEY, 
+    id_articulo int(6),
+    id_usuario INT (4),
+    contenido VARCHAR(255),
+    fecha_hora DATETIME,
 
 );
